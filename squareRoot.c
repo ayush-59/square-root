35bc29dfdb9850a167c938844c5b6a1f00330920
Ayush Singh <60854318+ayush-59@users.noreply.github.com>
2021-07-18 12:34:27 +0530

    Create squareRoot.c

---
+++ squareRoot.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <math.h>
+
+float squareRoot(float n, float left, float right, int p){
+    float mid = (left + right) / 2;
+
+    if(fabs(mid*mid - n) < pow(10, -p) )
+        return mid;
+    
+    if(mid*mid < n)
+        return squareRoot(n, mid, right, p);
+    else
+        return squareRoot(n, left, mid, p);
+}
+
+int main()
+{
+    int n, p;
+    float res;
+    
+    scanf("%d", &n);
+    scanf("%d", &p);
+    
+    res = squareRoot(n, 0, n, p);
+    res = round(res*pow(10,p));
+    res = res/pow(10,p);
+    printf("ans = %.*f",p , res);
+
+    return 0;
+}
